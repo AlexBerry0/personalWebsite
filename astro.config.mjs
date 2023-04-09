c994413fb928f20deca4da8bec0084f4c9c4cb0c
Alex Berry <me@alex-berry.net>
2023-04-10 00:18:19 +1200

    v0.3
    Added a draft version of the blog section plus some bug fixes and padding changes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-});
+  integrations: [react()],
+  site: 'https://alex-berry.net'
+}
+);
+
---
+++ src/content/blog/Temppost.md
@@ -0,0 +1,8 @@
+---
+title: "Temp post"
+description: "Just a temporary post"
+pubDate: "Jul 15 2022"
+---
+
+
+This is just a temporary post till I actually write some propper ones
