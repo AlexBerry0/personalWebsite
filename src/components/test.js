ec5989be8809233c2abed017754be7ab52448a91
Alex Berry <me@alex-berry.net>
2023-05-24 09:15:08 +1200

    v0.4
    Redid the filesystem, redid the help command, redid the ascii image of me etc

---
+++ src/components/test.js
@@ -0,0 +1,23 @@
+const directories = {
+home: { 
+    Photos: {
+        testfile: "Never gonna give you up"
+            },
+    Desktop: {
+        Homework: {
+          test2: "Dam this is definitly a test file"  
+        }
+    },
+
+ },
+randomassfolder: 
+
+}
+
+var testnumber = 0
+
+if( testnumber == 2 ) {
+
+
+    
+}
--- src/content/blog/Temppost.md
+++
@@ -1,8 +0,0 @@
----
-title: "Temp post"
-description: "Just a temporary post"
-pubDate: "Jul 15 2022"
----
-
-
-This is just a temporary post till I actually write some propper ones
